101e904f42ea68872c1736f0efeee0deecc3a36f
Pavel <pzajtsev@yandex.ru>
2023-06-29 18:32:01 +0400

    Initial commit

---
+++ src/main/java/com/zpa/bit_tracker/BitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.zpa.bit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zpa/bit_tracker/BitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpa.bit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
