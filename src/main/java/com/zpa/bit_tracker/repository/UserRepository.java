2820fc18dc3b2d8013033f92a7a5ea7acc4205ed
Pavel <pzajtsev@yandex.ru>
2023-06-30 14:38:36 +0400

    создал сущность, контроллер, дто, репозиторий, сервис для юзера

---
+++ src/main/java/com/zpa/bit_tracker/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.zpa.bit_tracker.repository;
+
+import com.zpa.bit_tracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String name);
+}
